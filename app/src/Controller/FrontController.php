eb6dffa4f6755b6e49549e3a61ffb8f47459a1c5
Marcin Kazmierski <m.kazmierski@expans.io>
2021-05-09 09:57:33 +0200

    index template

--- app/src/Controller/FrontController.php
+++ app/src/Controller/FrontController.php
@@ -7,22 +7,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/")
- * Class FrontController
- * @package App\Controller
- */
+#[Route('/')]
 class FrontController extends AbstractController
 {
     /**
-     * @Route(
-     *     "",
-     *     name="index"
-     * )
      * @return Response
      */
+    #[Route('', name: 'index')]
     public function index(): Response
     {
-        return $this->render("bundles/TwigBundle/Exception/error403.html.twig", [], new Response('', Response::HTTP_FORBIDDEN));
+        return $this->render("front/index.html.twig", []);
     }
 }
