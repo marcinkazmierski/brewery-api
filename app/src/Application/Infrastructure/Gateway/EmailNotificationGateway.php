9fae7e8ee06c343c17d79fa38548fd62051c43dd
Marcin Kazmierski <m.kazmierski@expans.io>
2021-05-27 20:04:58 +0200

    Added mailer.

--- app/src/Application/Infrastructure/Gateway/EmailNotificationGateway.php
+++ app/src/Application/Infrastructure/Gateway/EmailNotificationGateway.php
@@ -7,11 +7,36 @@ namespace App\Application\Infrastructure\Gateway;
 use App\Application\Domain\Entity\User;
 use App\Application\Domain\Exception\ValidateException;
 use App\Application\Domain\Gateway\NotificationGatewayInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 class EmailNotificationGateway implements NotificationGatewayInterface
 {
+    private MailerInterface $mailer;
+
+    /**
+     * EmailNotificationGateway constructor.
+     * @param MailerInterface $mailer
+     */
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     public function userRegister(User $user): void
     {
         // TODO: Implement userRegister() method.
+        $email = (new TemplatedEmail())
+            ->from(new Address('zdalny-browar@kazmierski.com.pl', 'Zdalny Browar'))
+            ->to('marcin@kazmierski.com.pl')
+            ->subject('Time for Symfony Mailer!')
+            ->htmlTemplate('emails/registration.html.twig')
+            ->context([
+                'user' =>$user,
+            ]);
+
+        $this->mailer->send($email);
     }
 }
